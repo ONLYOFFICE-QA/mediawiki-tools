8e904adb8606a28607b1c79c7a2a2ce077bff603
Pavel Lobashov <shockwavenn@gmail.com>
2022-11-16 14:13:26 +0200

    Fix more

--- update.sh
+++ update.sh
@@ -16,4 +16,4 @@ cp -r /var/www/html/mediawiki/images/ .
 cp -r /var/www/html/mediawiki/extensions/ .
 cp -r /var/www/html/mediawiki/resources/assets/* ./resources/assets/
 mv /var/www/html/mediawiki /var/www/html/mediawiki-before-upgrade
-mv $TEMP_DIR/FULL_NAME /var/www/html/mediawiki
+mv $TEMP_DIR/$FULL_NAME /var/www/html/mediawiki
