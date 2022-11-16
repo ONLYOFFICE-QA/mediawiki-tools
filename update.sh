8f76bdd469410ec2fac34bf58d7af5b7e3a70cb9
Pavel Lobashov <shockwavenn@gmail.com>
2022-11-16 13:50:40 +0200

    Fix issues

--- update.sh
+++ update.sh
@@ -6,11 +6,11 @@ FULL_VERSION=$MAJOR_VERSION.$MINOR_VERSION
 FULL_NAME=mediawiki-$FULL_VERSION
 
 mkdir -pv $TEMP_DIR
-cd $TEMP_DIR || exit
+cd $TEMP_DIR || exit 1
 wget https://releases.wikimedia.org/mediawiki/$MAJOR_VERSION/$FULL_NAME.tar.gz
 tar -xvzf $FULL_NAME.tar.gz
 rm $FULL_NAME.tar.gz
-cd $TEMP_DIR/FULL_NAME || exit
+cd $TEMP_DIR/$FULL_NAME || exit 1
 cp /var/www/html/mediawiki/LocalSettings.php .
 cp -r /var/www/html/mediawiki/images/ .
 cp -r /var/www/html/mediawiki/extensions/ .
