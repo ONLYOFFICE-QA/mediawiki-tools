26b3c8dd938ea744835267cb754c9c6dc8542f06
Pavel Lobashov <shockwavenn@gmail.com>
2022-11-16 13:43:38 +0200

    Add first version of update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TEMP_DIR=/tmp/new-wiki
+MAJOR_VERSION=1.38
+MINOR_VERSION=4
+FULL_VERSION=$MAJOR_VERSION-$MINOR_VERSION
+FULL_NAME=mediawiki-$FULL_VERSION
+
+mkdir -pv $TEMP_DIR
+cd $TEMP_DIR || exit
+wget https://releases.wikimedia.org/mediawiki/$MAJOR_VERSION/$FULL_NAME.tar.gz
+tar -xvzf $FULL_NAME.tar.gz
+rm $FULL_NAME.tar.gz
+cd $TEMP_DIR/FULL_NAME || exit
+cp /var/www/html/mediawiki/LocalSettings.php .
+cp -r /var/www/html/mediawiki/images/ .
+cp -r /var/www/html/mediawiki/extensions/ .
+cp -r /var/www/html/mediawiki/resources/assets/* ./resources/assets/
+mv /var/www/html/mediawiki /var/www/html/mediawiki-before-upgrade
+mv $TEMP_DIR/FULL_NAME /var/www/html/mediawiki
